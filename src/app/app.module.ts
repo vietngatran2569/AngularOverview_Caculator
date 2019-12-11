bf125f30a1866e4e3e5489b702d7a01f6862ce25
vietngatran2569 <vietngatran2569@gmail.com>
2019-12-11 17:18:59 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CaculatorComponent } from './caculator/caculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CaculatorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/caculator/caculator.component.ts
@@ -0,0 +1,49 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-caculator',
+  templateUrl: './caculator.component.html',
+  styleUrls: ['./caculator.component.scss']
+})
+export class CaculatorComponent implements OnInit {
+  output: number;
+  first: number;
+  second: number;
+  operator = '+';
+
+  onFirstChange(value) {
+    this.first = Number(value);
+  }
+
+  onSecondChange(value) {
+    this.second = Number(value);
+  }
+
+  onSelectChange(value) {
+    this.operator = value;
+  }
+
+  calculate() {
+    switch (this.operator) {
+      case '+':
+        this.output = this.first + this.second;
+        break;
+      case '-':
+        this.output = this.first + this.second;
+        break;
+      case '*':
+        this.output = this.first * this.second;
+        break;
+      case '/':
+        this.output = this.first / this.second;
+        break;
+    }
+  }
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
